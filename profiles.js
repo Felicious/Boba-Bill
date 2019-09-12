018832d3d2f707fdf12004c7fd49f350eff72c04
Felicious <fkuan@scu.edu>
2019-09-12 01:22:18 -0700

    upload

---
+++ profiles.js
@@ -0,0 +1,13 @@
+var ppl = 0;
+
+<!--One question:
+if the user clicks on the button (which
+	opens the function more than once, would 
+	the global variable reset?
+Right now im assuming that the ppl var will keep
+	track of the total number of profiles
+	successfully created-->
+
+function CreateProfile() {
+  return a * a;
+}

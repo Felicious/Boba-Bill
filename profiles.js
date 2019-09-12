8729027b1ff50adbb6d3728daf422baa0723895b
Felicious <fkuan@scu.edu>
2019-09-12 02:29:34 -0700

    Create addTrans and paidBill class methods for Profile

--- profiles.js
+++ profiles.js
@@ -13,14 +13,34 @@ Right now im assuming that the ppl var will keep
 	Thinking of using classes-->
 
 class Profile {
+	//need unique id ...?
 	constructor(name) {
+		this.id: ppl; //global var
 		this.firstName: name; //user input. If blank, generate random name
-  		this.money: 0;
+  		this.money: 0; //neg if u owe group, positive if group owes u
   		this.transactionIds: [];
 	}
+
+	// Class Method: Adds Transaction Id and amount owed
+	// Update on Profile   
+	function addTransaction(id, owedAmt) {
+		this.transactionIds.push(id);
+		//how to link transaction id to owed amt??
+		this.money += owedAmt; //owedAmt is neg
+	}
+
+	// Class Method: when person pays for the bill
+	// Amount the person owes the group is deducted
+	function paidBill (paidAmt) {
+		this.money += paidAmt; 
 }
+
+
 
 
 var str = "profile" + String(ppl);
 let str = new Profile(user_input); //USER INPUT
 ppl += 1;
+
+//random names used for when the user leaves name empty
+var randBobaNames = ["Yuzu", "Krema", "Pearl", "Winter"];
